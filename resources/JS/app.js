014c7cd5778bc5fbed8dec419577346bd1f9e26c
Maxwell Alexander <alexander.maxwell84@gmail.com>
2022-05-31 13:25:53 -0500

    Se agrego el display y la animacion de las secciones

--- resources/JS/app.js
+++ resources/JS/app.js
@@ -1,14 +1,24 @@
 let sections = document.querySelectorAll('.section');
 let controlsSec = document.querySelectorAll('.controls');
 let controls = document.querySelectorAll('.control');
-let mainContent = document.querySelectorAll('.main-content');
+let mainContent = document.querySelector('.main-content');
+
+
+function displaySection(id){
+    let currentSection = document.querySelector('.active-sect');
+    currentSection.classList.remove('active-sect');
+    let actvSection = document.getElementById(id)
+    actvSection.classList.add('active-sect')
+}
 
 function pageTransition(){
-    for(let contol of controls){
-        contol.addEventListener('click', (event) => {
+    for(let control of controls){
+        control.addEventListener('click', event => {
             let currentBtn = document.querySelector('.active-btn');
             currentBtn.classList.remove('active-btn');
             event.target.classList.add('active-btn');
+            let id = event.target.dataset.id
+            displaySection(id)
         })
     }
 }
